ff9d3693610cd87b7db3dc36fd5e79a6c8db4c9f
Adam <adam@MacBook-Pro-Albert.local>
2019-06-09 14:27:12 +0200

    scraper modal addition

--- planner/scraper/__init__.py
+++ planner/scraper/__init__.py
@@ -8,18 +8,21 @@ def scrape(url):
 
 
 def lidl(url):
-    session = HTMLSession()
-    r = session.get(url)
-    ingredients_html = r.html.find('div.skladniki', first=True).find('li')
-    ingredients = []
-    for ingredient in ingredients_html:
-        ingredients.append(tuple(ingredient.text.split(' – ')))
-    name = r.html.find('div.lead', first=True).find('h1', first=True)
-    time = r.html.find('li.meta_time', first=True).find('strong', first=True)
-    # level = r.html.find('li.meta_level', first=True).find('em', first=True)
-    # porition_number = r.html.find('li.meta_size', first=True)
-    recipe_text = r.html.find('div.article#opis', first=True)
-    return {'name': name.text,
-            'time': time.text,
-            'text': recipe_text.text,
-            'ingredients': ingredients}
+    try:
+        session = HTMLSession()
+        r = session.get(url)
+        ingredients_html = r.html.find('div.skladniki', first=True).find('li')
+        ingredients = []
+        for ingredient in ingredients_html:
+            ingredients.append(tuple(ingredient.text.split(' – ')))
+        name = r.html.find('div.lead', first=True).find('h1', first=True)
+        time = r.html.find('li.meta_time', first=True).find('strong', first=True)
+        # level = r.html.find('li.meta_level', first=True).find('em', first=True)
+        # porition_number = r.html.find('li.meta_size', first=True)
+        recipe_text = r.html.find('div.article#opis', first=True)
+        return {'name': name.text,
+                'time': time.text,
+                'text': recipe_text.text,
+                'ingredients': ingredients}
+    except Exception as ex:
+        return None
--- planner/scraper/routes.py
+++ planner/scraper/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, url_for, request
 from flask_login import current_user, login_required
 
 from planner import db
@@ -9,16 +9,18 @@ from .forms import ScraperForm
 scrap = Blueprint('scrap', __name__)
 
 
-@scrap.route("/scraper", methods=['GET', 'POST'])
+@scrap.route("/scraper", methods=['POST'])
 @login_required
 def scraper():
     form = ScraperForm()
-    if form.validate_on_submit():
-        recipe_scr = scrape(form.recipe_url.data)
+    form.recipe_url.data = request.form.get('recipe_url')
+    recipe_scr = scrape(form.recipe_url.data)
+    if recipe_scr:
         recipe = Recipe(title=recipe_scr['name'], time=recipe_scr['time'], text=recipe_scr['text'], author=current_user)
         recipe.ingredients = [Ingredient(name=x, amount=y[0] if y else '') for x, *y in recipe_scr['ingredients']]
         db.session.add(recipe)
         db.session.commit()
-        flash(f'The recipe has been imported!', 'success') 
+        flash(f'The recipe has been imported!', 'success')
         return redirect(url_for('recipes.recipe', recipe_id=recipe.id))
+    flash(f'Something went wrong - please try again', 'danger')
     return render_template('scraper.html', title='Scraper', form=form)
